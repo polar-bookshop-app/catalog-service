6dd8eca39fdbaaf4be2397bfe862dc97010b01c8
Maksym Stepanenko <stepanenkomaksi@gmail.com>
2025-08-28 17:07:06 +0300

    Initial commit

---
+++ src/main/java/com/github/polar/catalog_service/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.polar.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
