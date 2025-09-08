5c0a49bd3dd5396362f7e2e81ce0a5080739364e
Maksym Stepanenko <stepanenkomaksi@gmail.com>
2025-09-08 15:06:32 +0300

    Minor changes

--- src/test/java/com/github/polar/catalogservice/TestCatalogServiceApplication.java
+++ src/test/java/com/github/polar/catalogservice/TestCatalogServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 public class TestCatalogServiceApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.from(CatalogServiceApplication::main)
                 .with(TestcontainersConfiguration.class)
                 .run(args);
