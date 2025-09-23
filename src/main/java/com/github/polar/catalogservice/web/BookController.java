9ebcba1b0b0943ef468e16c1a1e0ad24d06f04b6
Maksym Stepanenko <stepanenkomaksi@gmail.com>
2025-09-23 12:04:24 +0300

    Failed call emulation added

--- src/main/java/com/github/polar/catalogservice/web/BookController.java
+++ src/main/java/com/github/polar/catalogservice/web/BookController.java
@@ -3,13 +3,19 @@ package com.github.polar.catalogservice.web;
 import com.github.polar.catalogservice.domain.Book;
 import com.github.polar.catalogservice.domain.BookService;
 import jakarta.validation.Valid;
+import java.lang.invoke.MethodHandles;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
 public class BookController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private final BookService bookService;
 
@@ -24,22 +30,32 @@ public class BookController {
 
     @GetMapping("{isbn}")
     public Book getByIsbn(@PathVariable String isbn) {
+        LOGGER.info("getByIsbn {}", isbn);
+
+        // TODO: below added for testing purpose, to test failure mode
+        if ("0321349602".equals(isbn)) {
+            throw new IllegalStateException("Invalid ISBN");
+        }
+
         return bookService.viewBook(isbn);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book createBook(@Valid @RequestBody Book book) {
+        LOGGER.info("createBook {}", book.isbn());
         return bookService.addBook(book);
     }
 
     @DeleteMapping("{isbn}")
     public void deleteBook(@PathVariable String isbn) {
+        LOGGER.info("deleteBook {}", isbn);
         bookService.deleteBook(isbn);
     }
 
     @PutMapping("{isbn}")
     public Book updateOrCreateBook(@PathVariable String isbn, @Valid @RequestBody Book book) {
+        LOGGER.info("updateOrCreateBook {}", isbn);
         return bookService.updateOrCreateBook(isbn, book);
     }
 }
